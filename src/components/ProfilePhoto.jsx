27b0b3fb31c71199c952f6109b6e7ff1a5294ef9
Ceczzx <Geraldo123villanueva@gmail.com>
2026-05-03 12:01:56 -0500

    changes profile picture

--- src/components/ProfilePhoto.jsx
+++ src/components/ProfilePhoto.jsx
@@ -49,7 +49,7 @@ export default function ProfilePhoto() {
           }}
         >
           <img 
-            src="/Fotoyo.png" 
+            src="/foto elegante.jpg" 
             alt="Geraldo Villanueva"
             className="w-full h-full object-cover"
           />
